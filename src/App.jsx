2df6bd03f76775064a16b28d255318212b99db8a
yangxing <asdfgh12345625985@qq.com>
2023-03-14 22:32:27 +0800

    React17脚手架

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: yangxing
+ * @Date: 2023-03-14 22:03:12
+ * @LastEditors: yangxing
+ * @LastEditTime: 2023-03-14 22:26:04
+ */
+import "./App.css";
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
+}
